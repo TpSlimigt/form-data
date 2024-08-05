da8865a709b1be8d56cab82509a3dea004a88330
Viktor Andrén <viktor@osterlin.se>
2024-08-05 17:23:13 +0200

    Formated code

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,26 +1,29 @@
 'use client'
 
 type ButtonProps = {
-    label: string
-    color?: ColorVariantKey
-    onClick?: () => void
+  label: string
+  color?: ColorVariantKey
+  onClick?: () => void
 }
 
-type ColorVariantKey= 'blue' | 'white'
+type ColorVariantKey = 'blue' | 'white'
 
 export default function Button({
-    label,
-    color = 'blue',
-    onClick,
+  label,
+  color = 'blue',
+  onClick,
 }: ButtonProps): JSX.Element {
-    const colorVariants = {
-        blue: 'text-white bg-blue-500 hover:bg-blue-800',
-        white: 'text-blue-400 bg-white hover:bg-gray-100',
-    }
+  const colorVariants = {
+    blue: 'text-white bg-blue-500 hover:bg-blue-800',
+    white: 'text-blue-400 bg-white hover:bg-gray-100',
+  }
 
-    return(
-        <button
-        className={`focus:outline-none ${colorVariants[color]} font-medium text-sm px-3 py-2 ml-0 m-2`}
-        onClick={onClick}>{label}</button>
-    )
+  return (
+    <button
+      className={`focus:outline-none ${colorVariants[color]} m-2 ml-0 px-3 py-2 text-sm font-medium`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
 }
