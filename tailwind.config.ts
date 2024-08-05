0250b0fcc50fcd0e156f7416b7a1bf22bc86a81f
Viktor Andrén <viktor@osterlin.se>
2024-08-05 15:37:09 +0200

    Added web source for color gradiants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      // https://uicolors.app/create for color gradiants
       colors: {
         blue: {
           '50': '#f0f9ff',
